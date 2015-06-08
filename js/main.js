18487c729ca39e9a3c57bf597a8420934dc50464
Ramón Maturano <Ramon@Ramons-MBP.local>
2015-06-08 00:49:41 -0400

    WIP...

--- js/main.js
+++ js/main.js
@@ -2,11 +2,23 @@
 *	Shut The Box top level container
 */
 var ShutTheBox = React.createClass({
+	getInitialState: function () {
+		return {
+			diceResult: 0,
+			isDiceDisabled: false
+		}
+	},
+	onDiceRolled: function (diceResult) {
+		this.setState({ diceResult: diceResult });
+	},
+	onDisableDice: function (isDisabled) {
+		this.setState({ isDiceDisabled: isDisabled });
+	},
 	render: function () {
 		return (
 			<div className="shut-the-box-container">
-				<Box diceResult={this.props.diceres} />
-				<DiceBox />
+				<Box diceResult={this.state.diceResult} onDisableDice={this.onDisableDice} />
+				<Dice onDiceRolled={this.onDiceRolled}  isDiceDisabled={this.state.isDiceDisabled} />
 			</div>
 		);
 	}
@@ -15,93 +27,151 @@ var ShutTheBox = React.createClass({
 *	The Box component is responsible of rendering all the number tiles
 */
 var Box = React.createClass({
-
 	getInitialState: function () {
+		return { sum: 0, canEndTurn: false, tilesShut: [] };
+	},
+	sumAndValidate: function () {
+			// if this is a valid tile combination...
+			if (this.state.sum > 0 && this.state.sum <= this.props.diceResult)																																															 {
+				//can end Turn
+				this.setState({ canEndTurn: true }); //this is wrong according to the React way...
+			} else {
+				this.setState({ canEndTurn: false });
+				//check player can continue playing...
+			}
+	},
+	onEndTurn: function () {
+		this.render();
+		//I have disable the previously chosen tiles for good, but I don't have those numbers available here :/
+	},
+	render: function () {
+		//should this return a new props object?
+		this.sumAndValidate();
+
 		var tiles = [];
 
 		for (var i = 1; i <= 9; i++) {
-				tiles.push( { number: i,
-											"chosen": false,
-											"disabled": false }
-									);
+
+			var shouldTileBeDisabled =  this.state.tilesShut.indexOf(i) > -1 ? true : false
+
+			var props = { key: "tile-" + i,
+										number: i,
+										onDisableDice: this.props.onDisableDice,
+										isShut: this.state.tilesShut.indexOf(i) > -1 ? true : false
+									};
+
+			if (!shouldTileBeDisabled) {
+				props.onTileClick = this.onTileClick;
+			}
+
+			tiles.push(<Tile {...props} />);
 		}
 
-		return { tiles: tiles }
-	},
-
-	render: function () {
-		var tiles = this.state.tiles.map(function (value, key) {
-			return <Tile status={value.chosen} number={value.number} key={key} />
-		});
-
 		return (
-			<ul>{tiles}</ul>
+			<div>
+				<h2>{this.props.diceResult}</h2>
+				<h2>{this.state.sum}</h2>
+				<ul className="dice-list">{tiles}</ul>
+				<button disabled={this.state.canEndTurn ? '' : 'disabled'} onClick={this.onEndTurn}>End Turn</button>
+			</div>
 		);
+	},
+	/**
+	*
+	*/
+	onTileClick: function (num) {
+		var tilesShut = this.state.tilesShut,
+				targetIndex = this.state.tilesShut.indexOf(Math.abs(num));
+		//if not yet in tilesShut state
+		if (targetIndex === -1) {
+			tilesShut.push(num);
+		} else {
+			//meaning tile was open back up
+			tilesShut.splice(targetIndex, 1);
+		}
+
+		this.setState({ sum: this.state.sum + num, tilesShut: tilesShut});
 	}
-
-
-	//if 7,8 or 9 is still on the table must roll both dice
-	//else player can roll one die or two dice
-	//keep rolling dice until no boxes can be shut
-	//need to count the uncovered numbers from the boxes
 
 });
 
 /**
-*	Creates the DOM representation of a tile that lives inside the box
+*	Creates the DOM representation of a tile inside the Box
 */
 var Tile = React.createClass({
 
 	getInitialState: function () {
-		return { chosen: this.props.chosen };
+		return { chosen: false }
 	},
 
 	render: function () {
-		var chosenClassName = this.state.chosen ? "chosen" : "";
+		var tileClassNames = ['tile'];
 
-		return (
-			<li className={chosenClassName + " tile"}  onClick={this.onTileClick}>{this.props.number}</li>
-		);
+		if (this.state.chosen) {
+			tileClassNames.push('chosen');
+		}
+
+		if (this.props.isShut) {
+			tileClassNames.push('disabled');
+		}
+
+		return <li className={tileClassNames.join(' ')} onClick={this.onTileClick}>{this.props.number}</li>
 	},
 
 	onTileClick: function (e) {
-		//todo: push current clicked tile to a numbers data structure...
-		console.log(this.props.diceres)
+		var isChosen = !this.state.chosen;
 
-		this.setState({ chosen: !this.state.chosen });
+		this.setState({ chosen: isChosen });
+
+		if (isChosen) { //meaning we chose a new number
+			this.props.onTileClick(this.props.number);
+		} else {
+			//if it was previously chosen, substract its value
+			this.props.onTileClick(-this.props.number);
+		}
 	}
+
 
 });
 
-
-var DiceBox = React.createClass({
+var Dice = React.createClass({
 	getInitialState: function () {
 		return {
-			currentDiceNum: [6,6]
+			dice: [{value: 6, disabled: false }, {value: 6, disabled: false }],
+			isRollingDisabled: true
 		}
 	},
+
 	render: function () {
+		var disabledClass = this.state.dice[1].disabled ? 'disabled' : 	'';
+
 		return (
 			<div id="dice-container" className="dice-container">
 				<div className="dice">
-					{this.state.currentDiceNum[0]}
+					{this.state.dice[0].value}
 				</div>
 
-				<div className="dice">
-					{this.state.currentDiceNum[1]}
+				<div className={"dice " + disabledClass}>
+					{this.state.dice[1].value}
 				</div>
 
-				<button onClick={this.rollDice}>Roll Dice</button>
+				<button onClick={this.rollDice} disabled={!this.state.isRollingDisabled ? 'disabled' : ''}>Roll Dice</button>
 			</div>
 		);
 	},
+
 	rollDice: function () {
-		var dice1 = Math.floor(Math.random()*6),
-				dice2 = Math.floor(Math.random()*6);
+		var dice1 = Math.floor(1 + Math.random()*6),
+				dice2 = Math.floor(1 + Math.random()*6);
 
-		this.setState({ currentDiceNum: [dice1, dice2] });
+		this.setState({
+				currentDiceNum: [dice1, dice2]
+		});
 
-		this.props.diceResult = dice1 + dice2;
+		this.props.onDiceRolled(dice1 + dice2);
+
+		//disable roll dice button
+		this.setState({ isRollingDisabled: false })
 	}
 });
 
